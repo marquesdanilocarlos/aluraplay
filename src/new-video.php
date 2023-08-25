8ca1d957dfe614b8b803f15b1a3274d2a9ec4917
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-08-25 00:27:47 -0300

    Aula 01 - Listagem e inclusão de vídeos

---
+++ src/new-video.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Aluraplay\Database\Connection;
+
+$connection = Connection::getInstance();
+
+$data = filter_input_array(INPUT_POST, [
+    "url" => FILTER_VALIDATE_URL,
+    "title" => FILTER_SANITIZE_SPECIAL_CHARS
+]);
+
+$query = "INSERT INTO videos (title, url) VALUES (:title, :url)";
+$stmt = $connection->prepare($query);
+
+$result = $stmt->execute($data);
+
+if ($result) {
+    header("Location: /index.php");
+}
